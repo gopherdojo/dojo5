b951318a4e53e4475512c1881b82c3c14c0fda3e
fujiokayu <fujiokayu@gmail.com>
2019-06-03 23:18:13 +0900

    Add custom usage.

--- kadai1/fujiokayu/args/args.go
+++ kadai1/fujiokayu/args/args.go
@@ -2,7 +2,9 @@ package args
 
 import (
 	"flag"
+	"io"
 	"log"
+	"os"
 )
 
 //Args struct has parsed args.
@@ -12,8 +14,27 @@ type Args struct {
 	RootFolderName []string
 }
 
+func usage() {
+	_, err := io.WriteString(os.Stderr, usageString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	flag.PrintDefaults()
+}
+
+const usageString = `Usage of myConverter: 
+  # convert
+  ./myConverter [-from ext] [-to ext] directory
+
+  # example 
+  ./myConverter -from png -to jpg testdir
+  
+  # args
+`
+
 // ParseArgs is the constructor of struct "args"
 func ParseArgs() *Args {
+	flag.Usage = usage
 	arg1 := flag.String("from", "jpg", "original file type to convert")
 	arg2 := flag.String("to", "png", "file type you want to convert")
 
