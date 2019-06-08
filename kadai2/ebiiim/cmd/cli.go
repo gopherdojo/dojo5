d13220fc2675bb8c7d5b3d68043566554c69b206
ebiiim <mail@ebiiim.com>
2019-06-08 22:18:13 +0900

    update: error type assertion

--- kadai2/ebiiim/cmd/cli.go
+++ kadai2/ebiiim/cmd/cli.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pkg/errors"
+
 	"github.com/gopherdojo/dojo5/kadai2/ebiiim/pkg/conv"
 	"github.com/gopherdojo/dojo5/kadai2/ebiiim/pkg/dirconv"
 )
@@ -17,8 +19,23 @@ Arguments:
   -source_ext=<ext>` + "\t" + usageSrcExt + ` [default: jpg]
   -target_ext=<ext>` + "\t" + usageTgtExt + ` [default: png]`
 
+// NoArgsException is used when the program do not have args.
+type NoArgsException struct{ s string }
+
+// Error returns a NoArgsException error message.
+func (e *NoArgsException) Error() string { return e.s }
+
+// NoArgs is used by IsNoArgs to check the interface.
+func (e *NoArgsException) NoArgs() bool { return true }
+
+// IsNoArgs checks the given error implements NoArgsException or do not.
+func IsNoArgs(err error) bool {
+	b, ok := errors.Cause(err).(interface{ NoArgs() bool })
+	return ok && b.NoArgs()
+}
+
 // ParseArgs initializes a DirConv struct with given command line arguments.
-func ParseArgs(args []string) *dirconv.DirConv {
+func ParseArgs(args []string) (*dirconv.DirConv, error) {
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
 	var (
 		srcExt = flags.String("source_ext", "jpg", usageSrcExt)
@@ -26,19 +43,28 @@ func ParseArgs(args []string) *dirconv.DirConv {
 	)
 	err := flags.Parse(args[1:])
 	if err != nil {
-		panic(err)
+		return &dirconv.DirConv{}, err
 	}
+
 	dir := flags.Arg(0) // get the first dir name only
 
-	return &dirconv.DirConv{Dir: dir, SrcExt: conv.ParseImgExt(*srcExt), TgtExt: conv.ParseImgExt(*tgtExt)}
+	// if no args, return an NoArgsException indicates the program should show an usage message
+	if len(dir) == 0 {
+		return &dirconv.DirConv{}, &NoArgsException{"please show usage"}
+	}
+
+	return &dirconv.DirConv{Dir: dir, SrcExt: conv.ParseImgExt(*srcExt), TgtExt: conv.ParseImgExt(*tgtExt)}, nil
 }
 
 func main() {
-	dc := ParseArgs(os.Args)
-
-	// show help if no dir specified
-	if dc.Dir == "" {
-		fmt.Println(usage)
+	dc, err := ParseArgs(os.Args)
+	if err != nil {
+		if IsNoArgs(err) {
+			// show usage if no dir specified
+			fmt.Fprintf(os.Stdout, "%s\n", usage)
+		} else {
+			fmt.Fprintf(os.Stderr, "%v", err)
+		}
 		os.Exit(0)
 	}
 	dc.Convert()
--- kadai2/ebiiim/cmd/cli_test.go
+++ kadai2/ebiiim/cmd/cli_test.go
@@ -1,9 +1,8 @@
-package main_test
+package main
 
 import (
 	"testing"
 
-	"github.com/gopherdojo/dojo5/kadai2/ebiiim/cmd"
 	"github.com/gopherdojo/dojo5/kadai2/ebiiim/pkg/conv"
 	"github.com/gopherdojo/dojo5/kadai2/ebiiim/pkg/dirconv"
 )
@@ -17,13 +16,13 @@ func check(t *testing.T, got interface{}, want interface{}) {
 func TestParseArgs(t *testing.T) {
 	// normal: no options
 	args := []string{"imgconv", "../../testdata"}
-	dc := main.ParseArgs(args)
+	dc, _ := ParseArgs(args)
 	check(t, *dc, dirconv.DirConv{Dir: "../../testdata", SrcExt: conv.ImgExtJPEG, TgtExt: conv.ImgExtPNG})
 }
 
 func TestParseArgs2(t *testing.T) {
 	// normal: with options
 	args := []string{"imgconv", "-source_ext=PNG", "-target_ext=.tiff", "../../testdata"}
-	dc := main.ParseArgs(args)
+	dc, _ := ParseArgs(args)
 	check(t, *dc, dirconv.DirConv{Dir: "../../testdata", SrcExt: conv.ImgExtPNG, TgtExt: conv.ImgExtTIFF})
 }
