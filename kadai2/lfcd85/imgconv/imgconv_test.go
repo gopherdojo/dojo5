10a8344ba48ed1f81c2092cbc5733f0332b321fd
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-06-10 08:34:24 +0900

    Copy kadai1 to kadai2

---
+++ kadai2/lfcd85/imgconv/imgconv_test.go
@@ -0,0 +1,120 @@
+package imgconv
+
+import (
+	"testing"
+)
+
+func assertEq(t *testing.T, actual interface{}, expected interface{}) {
+	if actual != expected {
+		t.Errorf("actual: %v, expected: %v", actual, expected)
+	}
+}
+
+/* NOTE: This testing can work, but should be run in a sandbox.
+
+func assertNil(t *testing.T, obj interface{}) {
+	if obj != nil {
+		t.Errorf("actual: not nil, expected: nil")
+	}
+}
+
+func assertNotNil(t *testing.T, obj interface{}) {
+	if obj == nil {
+		t.Errorf("actual: nil, expected: not nil")
+	}
+}
+
+func TestConvert(t *testing.T) {
+	cases := []struct {
+		from     string
+		to       string
+		expected bool
+	}{
+		{"jpeg", "png", true},
+		{"png", "gif", true},
+		{"gif", "jpeg", true},
+		{"jpeg", "jpeg", false},
+		{"rb", "go", false},
+	}
+
+	for _, c := range cases {
+		err := Convert("../test", c.from, c.to)
+		if c.expected == true {
+			assertNil(t, err)
+		} else {
+			assertNotNil(t, err)
+		}
+	}
+}
+
+*/
+
+func TestGenerateOutputPath(t *testing.T) {
+	cases := []struct {
+		path     string
+		fmtTo    ImgFmt
+		expected string
+	}{
+		{
+			"path/to/hoge.jpg",
+			ImgFmt("png"),
+			"path/to/hoge.png",
+		},
+		{
+			"./path/to/fuga.PNG",
+			ImgFmt("jpeg"),
+			"./path/to/fuga.jpg",
+		},
+		{
+			"piyo.png",
+			ImgFmt("gif"),
+			"piyo.gif",
+		},
+	}
+
+	imgFmtExts.Init()
+	for _, c := range cases {
+		fmtTo = c.fmtTo
+		assertEq(t, generateOutputPath(c.path), c.expected)
+	}
+}
+
+func TestImgFmt_Detect(t *testing.T) {
+	cases := []struct {
+		extStr   string
+		expected ImgFmt
+	}{
+		{"png", ImgFmt("png")},
+		{"jpg", ImgFmt("jpeg")},
+		{"JPEG", ImgFmt("jpeg")},
+		{"GIF", ImgFmt("gif")},
+	}
+
+	imgFmtExts.Init()
+	var imgFmt ImgFmt
+	for _, c := range cases {
+		imgFmt.Detect(c.extStr)
+		assertEq(t, imgFmt, c.expected)
+	}
+}
+
+func TestImgFmt_Match(t *testing.T) {
+	cases := []struct {
+		fileName string
+		imgFmt   ImgFmt
+		expected bool
+	}{
+		{"hoge.jpg", ImgFmt("jpeg"), true},
+		{"fuga.png", ImgFmt("gif"), false},
+		{"piyo.png", ImgFmt("png"), true},
+		{"foo.js", ImgFmt("png"), false},
+		{".JPEG", ImgFmt("jpeg"), true},
+		{"jpeg", ImgFmt("jpeg"), false},
+		{"foopng", ImgFmt("png"), false},
+	}
+
+	imgFmtExts.Init()
+	for _, c := range cases {
+		assertEq(t, c.imgFmt.Match(c.fileName), c.expected)
+	}
+}
