a00817a3ad7a72571ff3d9365728aba52058d42a
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-06-11 00:01:19 +0900

    Add file existence checks to integration test

--- kadai2/lfcd85/imgconv/imgconv_test.go
+++ kadai2/lfcd85/imgconv/imgconv_test.go
@@ -2,6 +2,7 @@ package imgconv
 
 import (
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -23,27 +24,73 @@ func assertNotNil(t *testing.T, obj interface{}) {
 	}
 }
 
+func assertFileExists(t *testing.T, filePath string, expected bool) {
+	_, err := os.Stat(filePath)
+	actual := err == nil
+	if actual != expected {
+		switch expected {
+		case true:
+			t.Errorf("file %v should exist but does not", filePath)
+		case false:
+			t.Errorf("file %v should not exist but does", filePath)
+		}
+	}
+}
+
 func TestConvert(t *testing.T) {
 	cases := []struct {
-		from     string
-		to       string
-		expected bool
+		from              string
+		to                string
+		expectedSuccess   bool
+		expectedFileNames map[string]bool
 	}{
-		{"jpeg", "png", true},
-		{"png", "gif", true},
-		{"gif", "jpeg", true},
-		{"jpeg", "jpeg", false},
-		{"rb", "go", false},
+		{"jpeg", "png", true, map[string]bool{
+			"not_image.png":         false,
+			"not_image2.png":        false,
+			"sample1.png":           true,
+			"sample2.png":           true,
+			"sample4.png":           false,
+			"child_dir/sample3.png": true,
+			"child_dir/sample4.png": false,
+			"child_dir/sample5.png": false,
+		}},
+		{"png", "gif", true, map[string]bool{
+			"not_image.gif":         false,
+			"not_image2.gif":        false,
+			"sample1.gif":           false,
+			"sample2.gif":           false,
+			"sample4.gif":           true,
+			"child_dir/sample3.gif": false,
+			"child_dir/sample4.gif": true,
+			"child_dir/sample5.gif": false,
+		}},
+		{"gif", "jpeg", true, map[string]bool{
+			"not_image.jpg":         false,
+			"not_image2.jpg":        false,
+			"sample1.jpg":           false,
+			"sample2.jpg":           false,
+			"sample4.jpg":           false,
+			"child_dir/sample3.jpg": false,
+			"child_dir/sample4.jpg": false,
+			"child_dir/sample5.jpg": true,
+		}},
+		{"jpeg", "jpeg", false, nil},
+		{"rb", "go", false, nil},
 	}
 
+	outputDir := "./output/testdata"
 	for _, c := range cases {
 		defer os.RemoveAll("./output")
 
 		err := Convert("../testdata", c.from, c.to)
-		if c.expected == true {
+		if c.expectedSuccess == true {
 			assertNil(t, err)
 		} else {
 			assertNotNil(t, err)
+		}
+		for f, b := range c.expectedFileNames {
+			filePath := strings.Join([]string{outputDir, f}, "/")
+			assertFileExists(t, filePath, b)
 		}
 	}
 }
