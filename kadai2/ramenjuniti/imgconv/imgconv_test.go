09e754c674b52eef4920c9b3e5756a30c4639ae8
ramenjuniti <ramenjuniti@gmail.com>
2019-06-10 09:48:41 +0900

    add helper

--- kadai2/ramenjuniti/imgconv/imgconv_test.go
+++ kadai2/ramenjuniti/imgconv/imgconv_test.go
@@ -37,24 +37,26 @@ var tests = []struct {
 	},
 }
 
+func convertHelper(t *testing.T, path, out string) {
+	t.Helper()
+	img, err := Decode(path)
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+	err = img.Encode(out)
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+	}
+}
+
 func TestConvert(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			img, err := Decode(test.path)
-			if err != nil {
-				t.Fatalf("unexpected error %v", err)
-			}
-
-			err = img.Encode(test.out)
-			if err != nil {
-				t.Fatalf("unexpected error %v", err)
-			}
-
-			_, err = os.Stat(test.outFile)
+			convertHelper(t, test.path, test.out)
+			_, err := os.Stat(test.outFile)
 			if err != nil {
 				t.Fatalf("did not create %v", test.outFile)
 			}
-
 			err = os.Remove(test.outFile)
 			if err != nil {
 				t.Fatalf("cannot remove %v", test.outFile)
