8bf5744805c29f306d390075920840ca648ea4fd
fujiokayu <fujiokayu@gmail.com>
2019-06-11 23:18:39 +0900

    Add testDiv column into test table.

--- kadai2/fujiokayu/converter/converter_test.go
+++ kadai2/fujiokayu/converter/converter_test.go
@@ -24,20 +24,21 @@ func fileAssertionHelper(t *testing.T, testCase string, file string) {
 func Test_Convert(t *testing.T) {
 	cases := []struct {
 		testCase   string
+		testDiv    string
 		decodeFile string
 		encodeFile string
 		decodeType string
 		encodeType string
 	}{
-		{testCase: "J2P", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.png", decodeType: "jpg", encodeType: "png"},
-		{testCase: "J2G", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.gif", decodeType: "jpg", encodeType: "gif"},
-		{testCase: "P2J", decodeFile: "../testdata/cat.png", encodeFile: "../testdata/cat.jpg", decodeType: "png", encodeType: "jpg"},
-		{testCase: "P2G", decodeFile: "../testdata/cat.png", encodeFile: "../testdata/cat.gif", decodeType: "png", encodeType: "gif"},
-		{testCase: "G2J", decodeFile: "../testdata/cat.gif", encodeFile: "../testdata/cat.jpg", decodeType: "gif", encodeType: "jpg"},
-		{testCase: "G2P", decodeFile: "../testdata/cat.gif", encodeFile: "../testdata/cat.png", decodeType: "gif", encodeType: "png"},
-		{testCase: "errorCase", decodeFile: "../testdata/error", encodeFile: "../testdata/cat.png", decodeType: "gif", encodeType: "png"},
-		{testCase: "errorCase", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.png", decodeType: "tiff", encodeType: "png"},
-		{testCase: "errorCase", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.png", decodeType: "jpg", encodeType: "tiff"},
+		{testCase: "J2P", testDiv: "normal", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.png", decodeType: "jpg", encodeType: "png"},
+		{testCase: "J2G", testDiv: "normal", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.gif", decodeType: "jpg", encodeType: "gif"},
+		{testCase: "P2J", testDiv: "normal", decodeFile: "../testdata/cat.png", encodeFile: "../testdata/cat.jpg", decodeType: "png", encodeType: "jpg"},
+		{testCase: "P2G", testDiv: "normal", decodeFile: "../testdata/cat.png", encodeFile: "../testdata/cat.gif", decodeType: "png", encodeType: "gif"},
+		{testCase: "G2J", testDiv: "normal", decodeFile: "../testdata/cat.gif", encodeFile: "../testdata/cat.jpg", decodeType: "gif", encodeType: "jpg"},
+		{testCase: "G2P", testDiv: "normal", decodeFile: "../testdata/cat.gif", encodeFile: "../testdata/cat.png", decodeType: "gif", encodeType: "png"},
+		{testCase: "decode file not exists", testDiv: "error", decodeFile: "../testdata/error", encodeFile: "../testdata/cat.png", decodeType: "gif", encodeType: "png"},
+		{testCase: "undefined decode type", testDiv: "error", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.png", decodeType: "tiff", encodeType: "png"},
+		{testCase: "undefined encode type", testDiv: "error", decodeFile: "../testdata/cat.jpg", encodeFile: "../testdata/cat.png", decodeType: "jpg", encodeType: "tiff"},
 	}
 
 	for _, c := range cases {
@@ -45,13 +46,15 @@ func Test_Convert(t *testing.T) {
 		t.Run(c.testCase, func(t *testing.T) {
 			err := Convert(c.decodeFile, c.decodeType, c.encodeType)
 
-			if c.testCase != "errorCase" && err != nil {
+			if c.testDiv != "error" && err != nil {
 				t.Errorf(
 					"Case (%s) Failed test: Convert error.",
 					c.testCase)
 			}
+
 			fileAssertionHelper(t, c.testCase, c.encodeFile)
-			if c.testCase == "errorCase" && err == nil {
+
+			if c.testDiv == "error" && err == nil {
 				t.Errorf(
 					"Case (%s) Failed test: Convert error.",
 					c.testCase)
