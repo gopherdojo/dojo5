55849c772c1742bae46f21e6f37e9b13b6e6986f
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-06-15 16:53:04 +0900

    Add `words` package and its test

--- kadai3-1/lfcd85/go.mod
+++ kadai3-1/lfcd85/go.mod
@@ -1,3 +1,5 @@
 module github.com/gopherdojo/dojo5/kadai3-1/lfcd85
 
 go 1.12
+
+require github.com/hashicorp/go-multierror v1.0.0
---
+++ kadai3-1/lfcd85/words/words.go
@@ -0,0 +1,34 @@
+package words
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/gopherdojo/dojo5/kadai3-1/lfcd85/typinggame"
+	"github.com/hashicorp/go-multierror"
+)
+
+func Import(path string) (typinggame.Words, error) {
+	var words typinggame.Words
+	var result error
+
+	f, err := os.Open(path)
+	if err != nil {
+		result = multierror.Append(result, err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			result = multierror.Append(result, err)
+		}
+	}()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		words = append(words, s.Text())
+	}
+	if err := s.Err(); err != nil {
+		result = multierror.Append(result, err)
+	}
+
+	return words, result
+}
