421e15e544f446dda932a09ba24a24733a138384
ebiiim <mail@ebiiim.com>
2019-06-17 22:49:47 +0900

    add: timer_test.go

---
+++ kadai3-1/ebiiim/pkg/timer/timer_test.go
@@ -0,0 +1,31 @@
+package timer_test
+
+import (
+	"testing"
+
+	"github.com/gopherdojo/dojo5/kadai3-1/ebiiim/pkg/timer"
+)
+
+func TestMakeChannel(t *testing.T) {
+	cases := []struct {
+		name     string
+		timeSec  int
+		isClosed bool
+	}{
+		{name: "normal", timeSec: 2, isClosed: true},
+		{name: "+boundary", timeSec: 1, isClosed: true},
+		{name: "0sec", timeSec: 0, isClosed: true},
+		{name: "-boundary", timeSec: -1, isClosed: true},
+		{name: "-N", timeSec: -2, isClosed: true},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			timerCh := timer.MakeChannel(c.timeSec)
+			_, isNotClosed := <-timerCh
+			if isNotClosed == c.isClosed {
+				t.Error("channel status error")
+			}
+		})
+	}
+}
