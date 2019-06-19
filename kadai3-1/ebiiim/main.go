72bfd62c402f11e5142221ac48a30dec0750a6da
ebiiim <mail@ebiiim.com>
2019-06-19 11:26:02 +0900

    update: flag

--- kadai3-1/ebiiim/main.go
+++ kadai3-1/ebiiim/main.go
@@ -2,36 +2,37 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gopherdojo/dojo5/kadai3-1/ebiiim/pkg/quiz"
 	"github.com/gopherdojo/dojo5/kadai3-1/ebiiim/pkg/typing"
 	"github.com/pkg/errors"
 )
 
-func main() {
+func game(quizFilePath string, timeSec int, randSeed int64) {
 	bc := context.Background()
 	ctx, cancel := context.WithCancel(bc)
 	defer cancel()
 	nextQuizCh := make(chan interface{})
 
 	// load quiz
-	// quizLoader := &quiz.DummyLoader{}
-	reader, err := os.Open("pkg/testdata/quiz.json")
+	reader, err := os.Open(quizFilePath)
 	if err != nil {
 		err = errors.Wrap(err, "failed to open file")
 		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
-	quizLoader, err := quiz.NewJSONLoader(reader, 1)
+	quizLoader, err := quiz.NewJSONLoader(reader, randSeed)
 	if err != nil {
 		err = errors.Wrap(err, "failed to generate JSONLoader")
 		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	tg := typing.NewTypingGame(ctx, nextQuizCh, quizLoader, os.Stdin, 5)
+	tg := typing.NewTypingGame(ctx, nextQuizCh, quizLoader, os.Stdin, timeSec)
 
 	// request the first quiz
 	nextQuizCh <- struct{}{}
@@ -52,5 +53,23 @@ QuizLoop:
 
 	// show the result
 	grade := tg.CalcGrade()
-	fmt.Fprintf(os.Stdout, "\nTotal Time: %d sec.\nAnswered: %d\nCorrects: %d\nCorrect Rate: %.0f%%\nWPM: %.0f\n", grade.TotalTime, grade.Answered, grade.Corrects, 100*grade.CorrectRate, grade.WordsPerMinute)
+	fmt.Fprintf(os.Stdout, "\nTotal Time: %d s\nAnswered: %d\nCorrects: %d\nCorrect Rate: %.0f%%\nWPM: %.0f\n", grade.TotalTime, grade.Answered, grade.Corrects, 100*grade.CorrectRate, grade.WordsPerMinute)
+}
+
+func main() {
+	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flags.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-quiz] [-time]\n", os.Args[0])
+		flags.PrintDefaults()
+	}
+	var (
+		timeSec      = flags.Int("time", 30, "Seconds the game takes.")
+		quizFilePath = flags.String("quiz", "pkg/testdata/quiz.json", "The path of quiz file.")
+	)
+	err := flags.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+	game(*quizFilePath, *timeSec, time.Now().UnixNano())
 }
