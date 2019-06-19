95c864d1675f06419a4cd653f07f9d14390ff3bd
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-06-19 09:45:11 +0900

    Implement minimum download function

--- kadai3-2/lfcd85/cmd/main.go
+++ kadai3-2/lfcd85/cmd/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"net/url"
+
 	"github.com/gopherdojo/dojo5/kadai3-2/lfcd85/mypget"
 )
 
 func main() {
-	mypget.Execute()
+	// FIXME: get URL from option
+	url, err := url.Parse("https://golang.org/doc/gopher/frontpage.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = mypget.New(url).Execute()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
--- kadai3-2/lfcd85/mypget/mypget.go
+++ kadai3-2/lfcd85/mypget/mypget.go
@@ -2,8 +2,47 @@ package mypget
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
 )
 
-func Execute() {
+type Downloader struct {
+	url *url.URL
+}
+
+func New(url *url.URL) *Downloader {
+	return &Downloader{
+		url: url,
+	}
+}
+
+func (d *Downloader) Execute() error {
 	fmt.Println("Hello, split downloader!")
+	err := d.Download()
+	return err
+}
+
+func (d *Downloader) Download() error {
+	req, err := http.NewRequest("GET", d.url.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// FIXME: create proper directory for downloading
+	f, err := os.Create("./output.jpg")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	return err
 }
