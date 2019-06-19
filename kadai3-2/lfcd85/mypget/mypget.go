8f5dc07668a55504e4bf2f81c26b5b1c27f345db
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2019-06-20 07:27:40 +0900

    Enable to split download length to ranges

--- kadai3-2/lfcd85/mypget/mypget.go
+++ kadai3-2/lfcd85/mypget/mypget.go
@@ -40,6 +40,9 @@ func (d *Downloader) download() error {
 	if !acceptBytesRanges(resp) {
 		return errors.New("split download is not supported in this response")
 	}
+	length := int(resp.ContentLength)
+	ranges := splitToRanges(length)
+	fmt.Println(ranges) // TODO: split downloading by ranges
 
 	// FIXME: create proper directory for downloading
 	f, err := os.Create("./output.jpg")
@@ -54,4 +57,26 @@ func (d *Downloader) download() error {
 
 func acceptBytesRanges(resp *http.Response) bool {
 	return resp.Header.Get("Accept-Ranges") == "bytes"
+}
+
+func splitToRanges(length int) []string {
+	rangeNum := 4 // FIXME: dynamically get range's number
+
+	var ranges []string
+	var rangeStart, rangeEnd int
+	rangeLength := length / rangeNum
+
+	for i := 0; i < rangeNum; i++ {
+		if i != 0 {
+			rangeStart = rangeEnd + 1
+		}
+		rangeEnd = rangeStart + rangeLength
+
+		if i == rangeNum - 1 && rangeEnd != length {
+			rangeEnd = length
+		}
+
+		ranges = append(ranges, fmt.Sprintf("bytes=%d-%d", rangeStart, rangeEnd))
+	}
+	return ranges
 }
